e72d1fd8ab6ecb2164df99be4c0d4cd3179bc3b5
Jonathan Müller <jonathanmueller.dev@gmail.com>
2016-11-04 19:43:06 +0100

    Add narrow cast for built-in types

--- include/type_safe/narrow_cast.hpp
+++ include/type_safe/narrow_cast.hpp
@@ -36,27 +36,42 @@ namespace type_safe
             using type = floating_point<T>;
         };
 
-        template <typename Target, typename Source, class Policy>
-        TYPE_SAFE_FORCE_INLINE constexpr bool is_narrowing(const integer<Source, Policy>& source)
+        template <typename Target, typename Source>
+        TYPE_SAFE_FORCE_INLINE constexpr bool is_narrowing(
+            const Source& source,
+            typename std::enable_if<std::is_integral<Source>::value, int>::type = 0)
         {
-            using target_t = typename get_target_integer<Target, Policy>::type::integer_type;
-            using limits   = std::numeric_limits<target_t>;
-            return sizeof(target_t) < sizeof(Source) // no narrowing possible
+            using limits = std::numeric_limits<Target>;
+            return sizeof(Target) < sizeof(Source) // no narrowing possible
                    && (source > Source(limits::max())
                        || source < Source(limits::min())); // otherwise check bounds
         }
 
         template <typename Target, typename Source>
-        TYPE_SAFE_FORCE_INLINE constexpr bool is_narrowing(const floating_point<Source>& source)
+        TYPE_SAFE_FORCE_INLINE constexpr bool is_narrowing(
+            const Source& source,
+            typename std::enable_if<std::is_floating_point<Source>::value, int>::type = 0)
+
         {
-            using target_t = typename get_target_floating_point<Target>::type::floating_point_type;
-            return sizeof(target_t) < sizeof(Source) // no narrowing possible
+            return sizeof(Target) < sizeof(Source) // no narrowing possible
                    // cast source -> underlying float -> target float -> source
                    // and check if it changed the value
-                   && static_cast<Source>(static_cast<target_t>(static_cast<Source>(source)))
-                          != static_cast<Source>(source);
+                   && static_cast<Source>(static_cast<Target>(source)) != source;
         }
     } // namespace detail
+
+    /// \returns An arithmetic type with the same value as in `source`,
+    /// but converted to to a different type.
+    /// \requires The value of `source` must be representable by the new target type.
+    template <typename Target, typename Source,
+              typename = typename std::enable_if<std::is_arithmetic<Source>::value>::type>
+    TYPE_SAFE_FORCE_INLINE constexpr Target narrow_cast(const Source& source) noexcept
+    {
+        return detail::is_narrowing<Target>(source) ?
+                   (DEBUG_UNREACHABLE(detail::assert_handler{}, "conversion would truncate value"),
+                    Target()) :
+                   static_cast<Target>(source);
+    }
 
     /// \returns A [type_safe::integer]() with the same value as `source` but of a different type.
     /// \requires The value of `source` must be representable by the new target type.
@@ -69,10 +84,7 @@ namespace type_safe
     {
         using target_integer = typename detail::get_target_integer<Target, Policy>::type;
         using target_t       = typename target_integer::integer_type;
-        return detail::is_narrowing<Target>(source) ?
-                   (DEBUG_UNREACHABLE(detail::assert_handler{}, "conversion would truncate value"),
-                    target_t()) :
-                   target_integer(static_cast<target_t>(static_cast<Source>(source)));
+        return narrow_cast<target_t>(static_cast<Source>(source));
     }
 
     /// \returns A [type_safe::floating_point]() with the same value as `source` but of a different type.
@@ -85,10 +97,7 @@ namespace type_safe
     {
         using target_float = typename detail::get_target_floating_point<Target>::type;
         using target_t     = typename target_float::floating_point_type;
-        return detail::is_narrowing<Target>(source) ?
-                   (DEBUG_UNREACHABLE(detail::assert_handler{}, "conversion would truncate value"),
-                    target_t()) :
-                   target_float(static_cast<target_t>(static_cast<Source>(source)));
+        return narrow_cast<target_t>(static_cast<Source>(source));
     }
 } // namespace type_safe
 
