dc9bc6fc6711067bac8d74829d6ae36042523e7c
Paul Fultz II <pfultz2@yahoo.com>
2017-02-12 14:48:16 -0600

    Install the library with cmake (#32)
    * Install the library and try to use the installed debug_assert if available
    
    * Add build testing variable
    
    * Only export target when using imported targets
    
    * Move find_package to external.cmake

--- external/external.cmake
+++ external/external.cmake
@@ -2,6 +2,12 @@
 # This file is subject to the license terms in the LICENSE file
 # found in the top-level directory of this distribution.
 
-execute_process(COMMAND git submodule update --init -- external/debug_assert
-                WORKING_DIRECTORY ${CMAKE_SOURCE_DIR})
-add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/external/debug_assert)
+find_package(debug_assert)
+if(debug_assert_FOUND)
+    set(TYPE_SAFE_HAS_IMPORTED_TARGETS On)
+else()
+    set(TYPE_SAFE_HAS_IMPORTED_TARGETS Off)
+    execute_process(COMMAND git submodule update --init -- external/debug_assert
+                    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR})
+    add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/external/debug_assert)
+endif()
