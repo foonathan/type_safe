5f4b75d2ffbe2e411efb95b3694a21fa636fb3e0
NN <nn1436401@gmail.com>
2020-03-26 13:35:50 +0200

    Workaround MSVC 16.20 bug.

--- include/type_safe/reference.hpp
+++ include/type_safe/reference.hpp
@@ -568,7 +568,7 @@ public:
     /// \exclude
     template <typename Return2, typename... Args2>
     function_ref(Return2 (*fptr)(Args2...),
-                 typename detail::enable_matching_function<decltype(fptr), Return, Args...>::type
+                 typename detail::enable_matching_function<Return2(*)(Args2...), Return, Args...>::type
                  = 0)
     : function_ref(detail::matching_function_pointer_tag{}, fptr)
     {}
@@ -617,7 +617,8 @@ public:
     /// std::string`, even though such a conversion would be ill-formed otherwise. \param 1 \exclude
     template <typename Return2, typename... Args2>
     explicit function_ref(const function_ref<Return2(Args2...)>& other,
-                          detail::enable_matching_function<decltype(other), Return2, Args2...> = 0)
+                          detail::enable_matching_function<const function_ref<Return2(Args2...)>&,
+                                                           Return2, Args2...> = 0)
     : storage_(other.storage_), cb_(other.cb_)
     {}
 
